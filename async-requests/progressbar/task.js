271508a2a32ba9ed606bde6edbb9da28bc10f107
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2022-11-16 17:31:45 +0300

    task 7 3/3

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,9 +2,12 @@ const progress = document.getElementById('progress');
 const send = document.getElementById('send');
 const xhr = new XMLHttpRequest();
 
-send.addEventListener('click', () => {
+send.addEventListener('click', (e) => {
+    e.preventDefault();
     xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
     xhr.send();
 
-    console.log(xhr.onreadystatechange);
+    xhr.onprogress = function (event){
+        progress.value += 0.01;
+    }
 });
